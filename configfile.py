9d3cfdfbc3f2f388527a8ae706d800e19da34d60
jayvzeex <84307234+jayvzeex@users.noreply.github.com>
2021-05-17 14:18:19 +0800

    Add files via upload

---
+++ configfile.py
@@ -0,0 +1,39 @@
+import sys
+
+token_address=''
+decimals='18'
+bnb_amount='0.0001'
+speedms='0'
+sec_wait='1'
+max_slippage='10'
+gwei='15'
+infura_url='https://bsc-dataseed3.defibit.io/'
+addedurl=''
+enableprofitsell='1'
+pprofitsell='10'
+
+enable_waiting='1'
+liq_before='1'
+approvetoken='1'
+
+enable_pricebuying='1'
+enable_priceselling='1'
+enablesellslippage='1'
+onlysell='0'
+
+gasusage='70000'
+sellslippage='40'
+pricebuy='2'
+pricesell='0.5'
+selltimesenable='0'
+selltimes='1'
+
+
+version='2'
+amount_threads='1'
+getclipboard='1'
+my_address = ''
+my_pk = ''
+
+
+sys.path.append(".")
